e22d64e57afd6e3bd8372f02c754ce94a03fb533
J55NH22 <joon7011@woorizen.com>
2020-09-16 15:41:15 +0900

    dart연습

--- dart_2/bin/dart_2.dart
+++ dart_2/bin/dart_2.dart
@@ -12,5 +12,31 @@ void main() {
     }
     list.add(data);
   }
-  print(list);
+  stdout.write(list);
+
+  print('\n\n1.범위설정  2.검색');
+  String check = stdin.readLineSync();
+  switch (check) {
+    case '1':
+      print('어디까지?');
+      int count = stdin.readByteSync();
+
+      print('\n');
+      print(list.take(count - 48).toList());
+      print('\n');
+      break;
+    case '2':
+      print('검색할 단어를 입력하세요.');
+      String element = stdin.readLineSync();
+      if (list.contains(element) == true) {
+        print('\n');
+        print([element]);
+        print('\n');
+        break;
+      } else {
+        print('검색 결과가 없습니다.');
+        break;
+      }
+      break;
+  }
 }
